0a1800e766d955a3642a5c946a139bbfe398734f
Matteo Barbero <66015486+maiobarbero@users.noreply.github.com>
2025-10-06 13:35:05 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.maiobarbero.dev',
+  site: 'https://maiobarbero.dev',
   base: '/',
   integrations: [
     tailwind(),
